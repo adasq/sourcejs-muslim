1ebb80d47447d928f248791ac2e0f65dcedeb0f8
Adam Płócieniak <adam.plocieniak@pearson.com>
2016-08-23 15:08:16 +0200

    initial upload

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
-# sourcejs-muslim
-a
+# Find out, how muslims see your component
+
+Adds new features to SourceJS.
+
+[SourceJS](http://sourcejs.com) plugin.
+
+## Install
+
+To install plugin, run npm command in `sourcejs/user` folder:
+
+```
+npm install sourcejs-muslim --save
+```
+
+Then run Grunt update in SourceJS root:
+
+```
+cd ..
+grunt update
+```
+
+After restarting your app, plugin will be loaded automatically. To disable it, remove npm plugin and run `grunt update` again.
+
+## Other SourceJS plugins
+
+* https://github.com/sourcejs/sourcejs-bubble
+* https://github.com/sourcejs/sourcejs-spec-dependencies
+* https://github.com/sourcejs/sourcejs-spec-status
+* https://github.com/sourcejs/sourcejs-specs-linting
---
+++ assets/index.js
@@ -0,0 +1,51 @@
+'use strict';
+
+define([
+    'jquery',
+    'sourceModules/module',
+], function ($, module) {
+
+    
+    function Ngdirective() {
+        this.init();
+    }
+
+    Ngdirective.prototype = module.createInstance();
+    Ngdirective.prototype.constructor = Ngdirective;
+
+    Ngdirective.prototype.init = function () {
+
+        var ENABLE_CLASS_NAME = 'source_slider_frame__on';
+
+        var ENABLE_MUSLIM_OPT_TEMPLATE = ''+
+        '<div class="source_main_nav_ac_item">'+
+        '    <div class="source_main_nav_ac_tx">Enable muslims</div>'+
+        '    <div class="source_slider_frame">'+
+        '        <span class="source_slider_button">'+
+        '            <span class="source_slider_text source_slider_text_on">ON</span>'+
+        '            <span class="source_slider_text source_slider_text_off">OFF</span>'+
+        '        </span>'+
+        '    </div>'+
+        '</div>';
+
+        var muslimOptElem = $(ENABLE_MUSLIM_OPT_TEMPLATE);
+        var bodyElem = $(document.body);
+        var muslimOptButtonElem = muslimOptElem.find('.source_slider_frame');
+        
+        muslimOptButtonElem.on('click', function(){
+            var isEnabled = muslimOptButtonElem.hasClass(ENABLE_CLASS_NAME);
+            if(isEnabled){
+                muslimOptButtonElem.removeClass(ENABLE_CLASS_NAME);
+                bodyElem.attr('dir', 'ltr');
+            }else{
+                muslimOptButtonElem.addClass(ENABLE_CLASS_NAME);
+                bodyElem.attr('dir', 'rtl');
+            }
+        });
+
+        $(document).ready(function(){
+            $('.source_main_nav_ac').append(muslimOptElem);
+        });
+    };
+    return new Ngdirective();
+});
