d5b32ebae8cc3ec0855d7e6852b9b02db11227fe
Adam Płócieniak <adam.plocieniak@pearson.com>
2016-08-26 15:12:45 +0200

    code simplified

--- assets/index.js
+++ assets/index.js
@@ -3,49 +3,30 @@
 define([
     'jquery',
     'sourceModules/module',
-], function ($, module) {
+    "sourceModules/innerNavigation",
+], function ($, module, innerNavigation) {
 
-    
-    function Ngdirective() {
+    function ArabicModule() {
         this.init();
     }
 
-    Ngdirective.prototype = module.createInstance();
-    Ngdirective.prototype.constructor = Ngdirective;
+    ArabicModule.prototype = module.createInstance();
+    ArabicModule.prototype.constructor = ArabicModule;
 
-    Ngdirective.prototype.init = function () {
+    ArabicModule.prototype.init = function () {
 
-        var ENABLE_CLASS_NAME = 'source_slider_frame__on';
+        var bodyElem, OPT_TEXT = 'Enable arabic';
 
-        var ENABLE_MUSLIM_OPT_TEMPLATE = ''+
-        '<div class="source_main_nav_ac_item">'+
-        '    <div class="source_main_nav_ac_tx">Enable arabic</div>'+
-        '    <div class="source_slider_frame">'+
-        '        <span class="source_slider_button">'+
-        '            <span class="source_slider_text source_slider_text_on">ON</span>'+
-        '            <span class="source_slider_text source_slider_text_off">OFF</span>'+
-        '        </span>'+
-        '    </div>'+
-        '</div>';
+        innerNavigation.addMenuItem(OPT_TEXT, enableArabic, disableArabic, '');
+        bodyElem = $(document.body);
+        
+        function enableArabic(){
+            bodyElem.attr('dir', 'rtl');
+        }
 
-        var muslimOptElem = $(ENABLE_MUSLIM_OPT_TEMPLATE);
-        var bodyElem = $(document.body);
-        var muslimOptButtonElem = muslimOptElem.find('.source_slider_frame');
-
-        muslimOptButtonElem.on('click', function(){
-            var isEnabled = muslimOptButtonElem.hasClass(ENABLE_CLASS_NAME);
-            if(isEnabled){
-                muslimOptButtonElem.removeClass(ENABLE_CLASS_NAME);
-                bodyElem.attr('dir', 'ltr');
-            }else{
-                muslimOptButtonElem.addClass(ENABLE_CLASS_NAME);
-                bodyElem.attr('dir', 'rtl');
-            }
-        });
-
-        $(document).ready(function(){
-            $('.source_main_nav_ac').append(muslimOptElem);
-        });
+        function disableArabic(){
+            bodyElem.attr('dir', 'ltr');
+        }
     };
-    return new Ngdirective();
+    return new ArabicModule();
 });
